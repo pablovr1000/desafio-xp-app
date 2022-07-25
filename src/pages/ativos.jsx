2f3cb914ea6fffc9a2e0fadc12ae3356bccb28d7
pablovr1000 <pablovr200@hotmail.com>
2022-07-24 22:13:58 -0300

    dev: Adicionado alguns estilos

--- src/pages/ativos.jsx
+++ src/pages/ativos.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { titulos } from '../data/titulos';
 import xpContext from '../context/xpContext';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import SwingTrade from '../components/swingTrade';
+/* import SwingTrade from '../components/swingTrade'; */
+import './Ativos.css'
 
 export default function Ativos() {
   const { userData, titulosUser, titulosGeral, setTitulosUser, setTitulosGeral } = useContext(xpContext);
+  const [compraVenda, setCompraVenda] = useState(false);
+  const [assetView, setAssetView] = useState(true);
+  const [assetAmount, setAssetAmount] = useState();
+  const [assetValue, setAssetValue] = useState();
+  const [assetInNegociation, setAssetInNegociation] = useState({});
 
   useEffect(() => {
     const ativosUser = userData.ativos;
@@ -39,76 +45,199 @@ export default function Ativos() {
     }
   }, []);
 
-  const handleClick = () => {
-    console.log('clicou');
+  const handleClickCompra = (value) => {
+    setCompraVenda(true);
+    setAssetView(false);
+    const value2 = parseInt(value);
+    var asset = titulosUser.find((element) => Object.values(element)[0] === value2);
+    if (!asset) asset = titulosGeral.find((element) => Object.values(element)[0] === value2);
+    setAssetInNegociation(asset);
+  };
+
+  const handleClickVenda = (value) => {
+    setCompraVenda(true);
+    setAssetView(false);
+    const value2 = parseInt(value);
+    var asset = titulosUser.find((element) => Object.values(element)[0] === value2);
+    if (!asset) asset = titulosGeral.find((element) => Object.values(element)[0] === value2);
+    setAssetInNegociation(asset);
+  };
+
+  const handleTrade = () => {
+    setAssetView(true);
+    setCompraVenda(false);
+    if(assetInNegociation.amount !== 0) {
+      
+    }
+    setAssetAmount();
+    setAssetValue();
+    setAssetInNegociation({});
+
   };
 
   return (
-    <div className="containerAtivos">
+    <div>
+      <div>
       <Header/>
-      <div className="formMeusAtivos">
-        <h3>Minhas Ações</h3>
-        {
-          titulosUser.map((asset)=> (
-            <tr className="formMeusAtivos">
-              <td className="ativo">{asset.titulo}</td>
-              <td className="quantidadeAtivo">{asset.amount}</td>
-              <td className="valorAtivo">{asset.valor}</td>
-              <td>
-                <button
-                  className="buttonCompraN"
-                  data-testid={`trade-compra-${asset.index}`}
-                  type="button"
-                  onClick={ handleClick }
-                >
-                  C
-                </button>
-              </td>
-              <td>  
-                <button
-                  className="buttonVendaN"
-                  data-testid={`trade-venda-${asset.index}`}
-                  type="button"
-                >
-                  V
-                </button>
-              </td>
-            </tr>
-          ))
-        }
-      </div>
-      <div className="formAtivosInvest">
-        <h3>Disponíveis para Investir</h3>
-        {
-          titulosGeral.map((asset)=> (
-            <tr className="formAtivosInvest">
-              <td className="ativo">{asset.titulo}</td>
-              <td className="valorAtivo">{asset.valor}</td>
-              <td>
-                <button
-                  className="buttonCompraN"
-                  data-testid="trade-compra-n"
-                  type="button"
-                >
-                  C
-                </button>
-              </td>
-            </tr>
-          ))
-        }
-      </div>
-      {/* <SwingTrade/> */}
-      <Link to="/movimentacao">
-        <div>
-          <button
-            className="buttonDepRet"
-            data-testid="button-dep-ret"
-            type="button"
-          >
-            Depósito/Retirada
-          </button>
+      { assetView === true ? 
+        <div className="containerAtivos">
+          <div className="formMeusAtivos">
+            <h3>Minhas Ações</h3>
+            <table className="table">
+              <tr className="formMeusAtivos">
+                <label className="label">Ação </label>
+                <label className="label">Qtde </label>
+                <label className="label">Preço </label>
+                {
+                  titulosUser.map((asset)=> (
+                    <div>
+                      <td className="td2">{asset.titulo}</td>
+                      <td className="td2">{asset.amount}</td>
+                      <td className="td2">{asset.valor}</td>
+                      <td className="td2">
+                        <button
+                          className="buttonCompraN"
+                          data-testid={`trade-compra-${asset.index}`}
+                          type="button"
+                          value={asset.index}
+                          onClick={ ({ target: { value } }) => handleClickCompra(value) }
+                        >
+                          C
+                        </button>
+                      </td>
+                      <td className="td2">  
+                        <button
+                          className="buttonVendaN"
+                          data-testid={`trade-venda-${asset.index}`}
+                          type="button"
+                          value={asset.index}
+                          onClick={ ({ target: { value } }) => handleClickVenda(value) }
+                        >
+                          V
+                        </button>
+                      </td>
+                    </div>
+                  ))
+                }
+              </tr>
+            </table>
+          </div>
+          <div className="formAtivosInvest">
+            <h3>Disponíveis para Investir</h3>
+            {
+              titulosGeral.map((asset)=> (
+                <tr className="formAtivosInvest">
+                  <td className="td">{asset.titulo}</td>
+                  <td className="td">{asset.valor}</td>
+                  <td className="td">
+                    <button
+                      className="buttonCompraN"
+                      data-testid="trade-compra-n"
+                      type="button"
+                      value={asset.index}
+                      onClick={ ({ target: { value } }) => handleClickCompra(value) }
+                    >
+                      C
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </div>
+          <br/><br/><br/>
+          <div className="salvoInfo">
+            <span className="saldo">Saldo em Conta: R$ </span>
+            <span className="saldoValor" data-testid="saldo">{ userData.saldoConta }</span>
+          </div>
+          <Link to="/movimentacao">
+            <div>             
+              <button
+                className="buttonDepRet"
+                data-testid="button-dep-ret"
+                type="button"
+              >
+                Depósito/Retirada
+              </button>
+            </div>
+          </Link>
         </div>
-      </Link>
+        :
+        <></>
+      }
+        <br/>
+        { compraVenda === true ? 
+          <div className="market-div">
+            <span>Comprar/Vender Ação</span>
+            <div className="assetInNegociation">
+              <div className="asset-in-div">
+                <label className="asset-in-label">Ação: </label>
+                <label className="asset-in-label">{ assetInNegociation.titulo }</label>
+              </div>
+              {
+                assetInNegociation.amount === 0 ?
+                  <></>
+                :
+                  <div className="asset-in-div">
+                    <label className="asset-in-label">Posição Atual: </label>
+                    <label className="asset-in-label">{ assetInNegociation.amount }</label>
+                  </div>
+              }
+              <div className="asset-in-div">
+                <label className="asset-in-label">Preço: </label>
+                <label className="asset-in-label">{ assetInNegociation.valor }</label>
+              </div>
+            </div>
+            <div>
+              <label>Quantidade: </label>
+              <input
+                className="inputAssetAmount"
+                name="inputAssetAmount"
+                data-testid="password-input"
+                placeholder="1 ~~ 99"
+                value={ assetAmount }
+                onChange={ ({ target: { value } }) => setAssetAmount(value) }
+              />
+            </div>
+            <div>
+              <label>Valor R$: </label>
+              <input
+                className="inputAssetValue"
+                name="inputAssetValue"
+                data-testid="password-input"
+                placeholder="Informe o Valor "
+                value={ assetValue }
+                onChange={ ({ target: { value } }) => setAssetValue(value) }
+              />
+            </div>
+            <div>
+              <button
+                className="buttonCompraN"
+                data-testid="trade-compra-n"
+                type="button"
+                onClick={ handleTrade }
+              >
+                Confirmar
+              </button>
+            </div>
+            <div>
+              <button
+                className="buttonCompraN"
+                data-testid="trade-compra-n"
+                type="button"
+                onClick={ () => {
+                  setAssetView(true);
+                  setCompraVenda(false);
+                  setAssetInNegociation({});
+                } }
+              >
+                Voltar
+              </button>
+            </div>
+          </div>
+          :
+          <></>
+        }
+      </div>
       <Footer/>
     </div>
   );
--- src/pages/saquesDepositos.jsx
+++ src/pages/saquesDepositos.jsx
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import xpContext from '../context/xpContext';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-/* import { useHistory } from 'react-router-dom'; */
 
 export default function Motimentacao() {
+  const { userData, setUserData } = useContext(xpContext);
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleTrade = (value) => {
+    var valor = userData;
+    const value2 = parseInt(inputValue);
+    if(value === 'retirar') {
+      valor.saldoConta = valor.saldoConta-value2;
+      setUserData(valor);
+      setInputValue(0);
+    } else {
+      valor.saldoConta = valor.saldoConta+value2;
+      setUserData(valor);
+      setInputValue(0);
+    }
+  };
   return (
     <div className="containerConta">
       <Header/>
       <form className="formMovimentacao">
         <div className="salvoInfo">
-          <span className="saldo">Saldo em Conta: </span>
-          <span className="saldoValor" data-testid="saldo">R$ 5.000,00</span>
+          <span className="saldo">Saldo em Conta: R$ </span>
+          <span className="saldoValor" data-testid="saldo">{ userData.saldoConta }</span>
         </div>
         <div className="operacao">
+          <input
+            className="valorOperacaoConta"
+            name="valorOperacaoConta"
+            data-testid="valorOperacaoConta"
+            value={ inputValue }
+            onChange={ ({ target: { value } }) => setInputValue(value) }
+          />
+        </div>
+        <div>
           <button
             className="deposito"
             data-testid="deposito-btn"
             type="button"
-            /* disabled={ isDisabled } */
+            value="retirar"
+            onClick={ ({ target: { value } }) => handleTrade(value) }
           >
-            Enter
+            Retirar
           </button>
-          <input
-          className="valorOperacaoConta"
-          name="valorOperacaoConta"
-          data-testid="valorOperacaoConta"
-          />
+          <button
+            className="confirmDepRet"
+            data-testid="confirm-dep-ret"
+            type="button"
+            value="depositar"
+            onClick={ ({ target: { value } }) => handleTrade(value) }
+          >
+            Depositar
+          </button>
         </div>
       </form>
       <Link to="/ativos">
@@ -39,16 +69,6 @@ export default function Motimentacao() {
           </button>
         </div>
       </Link>
-      <div>
-          <button
-            className="confirmDepRet"
-            data-testid="confirm-dep-ret"
-            type="button"
-            disabled="true"
-          >
-            Confirmar
-          </button>
-        </div>
       <Footer/>
     </div>
   );
